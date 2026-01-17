8f266d80c769a358d4317bf877eca1d67249e0e0
Zack-bug <zackn_roll@icloud.com>
2026-01-17 17:21:30 +0300

    Version 2

--- platformer.py
+++ platformer.py
@@ -1,151 +1,172 @@
 import pgzrun
-import math
 import random
 
 WIDTH= 800  
 HEIGHT= 500
 
-player = Actor('player_stand_r')
-walk_images_R = ['player_walk_r1','player_walk_r2']
-walk_images_L = ['player_walk_l1','player_walk_l2']
-player.pos = (WIDTH/2, HEIGHT/6)
-movementSpeed = 8
-velocity_y = 0
-gravity = 1
-frame_index=0
-direction = 'right'
-is_moving = False
-is_on_air = True
-has_lost = 0
+class Player(Actor):
+    def __init__(self):
+        super().__init__('player_stand_r1', (WIDTH /2,HEIGHT/6))
+        self.velocity_y = 0
+        self.gravity = 1.35
+        self.direction = 'right'
+        self.is_moving = False
+        self.is_on_air = True
+        self.idle_images_r = ['player_stand_r1','player_stand_r2']
+        self.idle_images_l = ['player_stand_l1','player_stand_l2']
+        self.walk_images_r = ['player_walk_r1', 'player_walk_r2']
+        self.walk_images_l = ['player_walk_l1', 'player_walk_l2']
+        self.frame_index = 0
+        
+    def move(self): 
+        self.is_moving=False
+        if keyboard.d:
+            self.x += self.movement_Speed
+            self.direction= 'right'
+            self.is_moving=True
+        if keyboard.a:
+            self.x -= self.movement_Speed
+            self.direction= 'left'
+            self.is_moving=True
+        if keyboard.w and not self.is_on_air:
+            self.velocity_y =-30
+            self.is_on_air =True 
+            if sound_on:
+                sounds.jump.play()   
 
-spinner = Actor('spinner1')
-spinner.images=['spinner1','spinner2']
-spinner.pos= (-200, HEIGHT-20)
-spinner_frame=0
-spinner_spawn_timer=0
-spinner_speed=4
+    def apply_gravity(self):    
+        self.y += self.velocity_y    
+        self.velocity_y += self.gravity
 
-fly = Actor('fly1')
-fly.images=['fly1','fly2']
-fly.pos= (0, 200)
-fly_frame=0
-fly_spawn_timer=0
-fly_speed=5
+        if self.bottom > HEIGHT-4:
+            self.bottom = HEIGHT
+            self.velocity_y = 0
+            self.is_on_air = False
+        else:
+            self.is_on_air=True
+
+    def animate(self):
+        
+        self.frame_index += 0.12
+        if self.frame_index >= len(self.walk_images_r):
+            self.frame_index = 0
+
+
+        if self.is_on_air:
+            if self.direction =='right':
+                self.image='player_air_r'
+            else:
+                self.image='player_air_l'    
+    
+        elif self.is_moving:
+            if self.direction=='right':       
+                self.image = self.walk_images_r[int(self.frame_index)]
+            elif self.direction=='left':       
+                self.image = self.walk_images_l[int(self.frame_index)]        
+        else: 
+            if self.direction=='right':
+                self.image= self.idle_images_r[int(self.frame_index)]
+            elif self.direction=='left':
+                self.image= self.idle_images_l[int(self.frame_index)]
+
+    def boundaries(self):
+
+        if self.right > WIDTH:
+            self.right=WIDTH
+        if self.left < 0:
+            self.left = 0
+        if self.top < 0:
+            self.top =0
+
+class Enemy(Actor):
+    def __init__(self, image, y, speed):
+        super().__init__(image,(-200,y))
+        self.speed = speed
+        self.spawn_timer=0
+        self.spawn_chance = random.randint(100,160)
+        self.direction = 'right'
+    def move(self):
+        self.x += self.speed
+        
+        if self.x>WIDTH+100 or self.x< -100:
+            self.spawn_timer+=1
+
+            if self.spawn_timer> self.spawn_chance:
+                self.spawn_timer=0
+                self.spawn_chance= random.randint(100,160)
+                if random.randint(0,1)==0:
+                    self.x=-50
+                    self.speed= abs(self.speed)
+                    self.direction='left'
+                else:
+                    self.x = WIDTH+50
+                    self.speed = -abs(self.speed)
+                    self.direction='right'
+class Spinner(Enemy):
+    def __init__(self):
+        super().__init__('spinner1', HEIGHT -20,4)
+        self.images = ['spinner1', 'spinner2']
+        self.frame_index=0
+    def animate(self):
+        self.frame_index +=0.2
+        if self.frame_index >= len(self.images):
+            self.frame_index=0
+        self.image = self.images[int(self.frame_index)]
+
+class Fly(Enemy):
+    def __init__(self):
+        super().__init__('fly1_r',220,6)
+        self.images_r= ['fly1_r', 'fly2_r']
+        self.images_l= ['fly1_l', 'fly2_l']
+        self.frame_index=0
+    def animate(self):
+        self.frame_index +=0.12
+        if self.frame_index >= len(self.images_r):
+            self.frame_index=0
+        
+        if self.direction == 'right':
+            self.image= self.images_r[int(self.frame_index)]
+        else:
+            self.image= self.images_l[int(self.frame_index)]
+player = Player()
+spinner= Spinner()
+fly = Fly()
 
 game_state= 'menu'
 sound_on=False
+has_lost = 0
+win_timer=0
+win_time_limit=3
+player_won=False
+
+
+
 play_button = Rect(WIDTH/2 -100, 305,200,50)
 sound_button= Rect(WIDTH/2 -100,365, 200,50)
 quit_button= Rect(WIDTH/2 -60, 425, 120,60)
 
 
 def update():
-    global velocity_y, frame_index, direction, is_moving, is_on_air, game_state, spinner_spawn_timer,spinner_speed,spinner_spawn_chance, fly_spawn_timer, fly_speed, fly_spawn_chance
+    global game_state
     
     if game_state =='menu':
         return
     
-    gravity = 1.35
-    is_moving=False
-    spinner.x+=spinner_speed
-    if keyboard.d:
-        player.x += movementSpeed
-        direction= 'right'
-        is_moving=True
-    if keyboard.a:
-        player.x -= movementSpeed
-        direction= 'left'
-        is_moving=True
-    if keyboard.w and not is_on_air:
-        velocity_y =-30
-        is_on_air =True 
-        if sound_on:
-             sounds.jump.play()   
-
-    player.y += velocity_y    
-    velocity_y += gravity
-
-    if player.bottom > HEIGHT-4:
-        player.bottom = HEIGHT
-        velocity_y = 0
-        is_on_air = False
-    else:
-        is_on_air=True
-
+    player.move()
+    player.apply_gravity()
+    player.animate()
+    player.boundaries()
     
-    frame_index += 0.12
-    if frame_index >= len(walk_images_R):
-       frame_index = 0
-
-
-    if is_on_air:
-        if direction =='right':
-            player.image='player_air_r'
-        else:
-            player.image='player_air_l'    
+    spinner.move()
+    spinner.animate()
+    fly.move()
+    fly.animate()
     
-    elif is_moving:
-        if direction=='right':       
-            player.image = walk_images_R[int(frame_index)]
-        elif direction=='left':       
-            player.image = walk_images_L[int(frame_index)]        
-
-    
-    elif direction=='right':
-        player.image='player_stand_r'
-    elif direction =='left':
-        player.image='player_stand_l'      
-
-    
-    if player.right > WIDTH:
-        player.right=WIDTH
-    if player.left < 0:
-        player.left = 0
-    if player.top < 0:
-        player.top =0
-
-
-    spinner.x += spinner_speed
-    if spinner.x > WIDTH + 100 or spinner.x < -100:
-        spinner_spawn_timer += 1
-        spinner_spawn_chance = random.randint(100,160)
-    
-        if spinner_spawn_timer > spinner_spawn_chance:
-            spinner_spawn_timer = 0
-            
-            if random.randint(0, 1) == 0:
-                    spinner.x = -50
-                    spinner_speed = 4
-            else:
-                spinner.x = WIDTH + 50
-                spinner_speed = -4
-    if player.colliderect(spinner):
+    if player.colliderect(spinner) or player.colliderect(fly):
         if sound_on:
             sounds.hit.play()
             sounds.hit.set_volume(.4)
         game_state = 'menu'
-        spinner_speed=0
-
-
-    fly.x += fly_speed
-    if fly.x > WIDTH + 100 or fly.x < -100:
-        fly_spawn_timer += 1
-        fly_spawn_chance = random.randint(100,160)
-        if fly_spawn_timer > fly_spawn_chance:
-            fly_spawn_timer = 0
-            
-            if random.randint(0, 1) == 0:
-                    fly.x = -0
-                    fly_speed = 5
-            else:
-                fly.x = WIDTH + 50
-                fly_speed = -5
-    if player.colliderect(fly):
-        game_state = 'menu'
-        if sound_on:
-             sounds.hit.play()  
-             sounds.hit.set_volume(.4)
-        fly_speed=0
 
 def on_mouse_down(pos):
     global game_state,sound_on
@@ -155,7 +176,10 @@ def on_mouse_down(pos):
         
         if play_button.collidepoint(pos):
             reset_game()
+            global player_won
             game_state='play'
+            player_won=False
+            clock.schedule_interval(update_timer,1)
         elif sound_button.collidepoint(pos):
             sound_on = not sound_on
             if sound_on:
@@ -169,21 +193,24 @@ def on_mouse_down(pos):
             quit()            
 
 def reset_game():
-    global spinner_speed, spinner_spawn_timer, velocity_y, is_on_air,has_lost, fly_speed, fly_spawn_timer
-
+    global has_lost,win_timer
+    clock.unschedule(update_timer)
     player.pos = (WIDTH/2, HEIGHT/6)
-    velocity_y = 0
-    is_on_air = True
+    player.velocity_y = 0
+    player.is_on_air = True
+    player.movement_Speed=6
 
     spinner.x = -200
-    spinner_speed = 4
-    spinner_spawn_timer = 0
-    
+    spinner.speed = 6
+    spinner.spawn_timer = 0
     fly.x=WIDTH+200
-    fly_speed=5
-    fly_spawn_timer=0
+    fly.speed=8
+    fly.spawn_timer=0
+
+    win_timer=0
     has_lost=1
 def draw():
+
     global has_lost
     screen.blit('background',(0,0))
 
@@ -194,13 +221,39 @@ def draw():
         screen.draw.text(sound_text, center=sound_button.center, fontsize=40, color='white')
         screen.draw.text("Quit", center = quit_button.center, fontsize=40, color= 'white')
         screen.draw.text("MUSHROOM ALIEN", center=(WIDTH/2,80), fontsize=65, color= 'white', shadow=(1,1))
-        if has_lost ==1:
-            screen.draw.text("Game Over", center=(WIDTH/2,160), fontsize=85, color= 'white', shadow=(1,1))
+        if has_lost ==1 and player_won==False:
+            screen.draw.text("Game Over", center=(WIDTH/2,160), fontsize=85, color= 'red', shadow=(1,1))
         
+    if player_won:
+        global win_timer
+        screen.draw.text(
+        "YOU WIN", center=(WIDTH/2,210), fontsize =90, color="green", shadow=(2,2))
 
     elif game_state=='play':
         player.draw()
         spinner.draw()
         fly.draw()
+        
+        screen.draw.text(
+            f"{win_timer} / {win_time_limit}",
+        topright=(WIDTH-20,20), fontsize=35, color = ('yellow'))
+
+
+def update_timer():
+    global win_timer, player_won, game_state
+    if game_state == 'menu':
+        return
+    if win_timer<win_time_limit:
+        win_timer+=1
+    else:
+        player_won=True
+        game_state='menu'
+        if sound_on:
+            sounds.win_sound.set_volume(.2)
+            sounds.win_sound.play()
+            
+        
+        win_timer=0
+
 pgzrun.go()
 
